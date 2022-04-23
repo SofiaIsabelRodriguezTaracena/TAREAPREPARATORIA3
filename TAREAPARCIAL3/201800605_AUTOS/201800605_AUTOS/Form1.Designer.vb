edf130f69e1b1fc5f26f4ba9011b038f90c7f8eb
Sofía Isabel Rodríguez Taracena 201800605 <98674890+SofiaIsabelRodriguezTaracena@users.noreply.github.com>
2022-04-23 17:14:25 -0600

    Add files via upload

---
+++ TAREAPARCIAL3/201800605_AUTOS/201800605_AUTOS/Form1.Designer.vb
@@ -0,0 +1,311 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form reemplaza a Dispose para limpiar la lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requerido por el Diseñador de Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
+    'Se puede modificar usando el Diseñador de Windows Forms.  
+    'No lo modifique con el editor de código.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.DataGridView1 = New System.Windows.Forms.DataGridView()
+        Me.Column1 = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.Column2 = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.Column3 = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.Column4 = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.Column5 = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.Column6 = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.TextBox3 = New System.Windows.Forms.TextBox()
+        Me.ComboBox1 = New System.Windows.Forms.ComboBox()
+        Me.MenuStrip1 = New System.Windows.Forms.MenuStrip()
+        Me.VaciarVectoresToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.VaciarDatagridToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.SalirToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.TextBox4 = New System.Windows.Forms.TextBox()
+        Me.Button4 = New System.Windows.Forms.Button()
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.MenuStrip1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.BackColor = System.Drawing.SystemColors.AppWorkspace
+        Me.Label1.Location = New System.Drawing.Point(21, 157)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(98, 13)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "TIPO DE CARROS"
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(364, 368)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(98, 34)
+        Me.Button1.TabIndex = 1
+        Me.Button1.Text = "Agregar"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.BackColor = System.Drawing.SystemColors.AppWorkspace
+        Me.Label2.Location = New System.Drawing.Point(398, 126)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(121, 13)
+        Me.Label2.TabIndex = 3
+        Me.Label2.Text = "KILOMETRAJE INICIAL"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.BackColor = System.Drawing.SystemColors.AppWorkspace
+        Me.Label3.Location = New System.Drawing.Point(398, 157)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(114, 13)
+        Me.Label3.TabIndex = 4
+        Me.Label3.Text = "KILOMETRAJE FINAL"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.BackColor = System.Drawing.SystemColors.AppWorkspace
+        Me.Label4.Location = New System.Drawing.Point(21, 126)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(41, 13)
+        Me.Label4.TabIndex = 5
+        Me.Label4.Text = "PLACA"
+        '
+        'DataGridView1
+        '
+        Me.DataGridView1.AllowUserToAddRows = False
+        Me.DataGridView1.AllowUserToDeleteRows = False
+        Me.DataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.DataGridView1.Columns.AddRange(New System.Windows.Forms.DataGridViewColumn() {Me.Column1, Me.Column2, Me.Column3, Me.Column4, Me.Column5, Me.Column6})
+        Me.DataGridView1.Location = New System.Drawing.Point(24, 212)
+        Me.DataGridView1.Name = "DataGridView1"
+        Me.DataGridView1.ReadOnly = True
+        Me.DataGridView1.Size = New System.Drawing.Size(643, 150)
+        Me.DataGridView1.TabIndex = 6
+        '
+        'Column1
+        '
+        Me.Column1.HeaderText = "numero de placa"
+        Me.Column1.Name = "Column1"
+        Me.Column1.ReadOnly = True
+        '
+        'Column2
+        '
+        Me.Column2.HeaderText = "tipo de auto"
+        Me.Column2.Name = "Column2"
+        Me.Column2.ReadOnly = True
+        '
+        'Column3
+        '
+        Me.Column3.HeaderText = "cobro base"
+        Me.Column3.Name = "Column3"
+        Me.Column3.ReadOnly = True
+        '
+        'Column4
+        '
+        Me.Column4.HeaderText = "kilometraje inicial"
+        Me.Column4.Name = "Column4"
+        Me.Column4.ReadOnly = True
+        '
+        'Column5
+        '
+        Me.Column5.HeaderText = "kilometraje final"
+        Me.Column5.Name = "Column5"
+        Me.Column5.ReadOnly = True
+        '
+        'Column6
+        '
+        Me.Column6.HeaderText = "pago final"
+        Me.Column6.Name = "Column6"
+        Me.Column6.ReadOnly = True
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(468, 368)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(98, 34)
+        Me.Button2.TabIndex = 7
+        Me.Button2.Text = "Eliminar"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Button3
+        '
+        Me.Button3.Location = New System.Drawing.Point(569, 368)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(98, 34)
+        Me.Button3.TabIndex = 8
+        Me.Button3.Text = "Actalizar"
+        Me.Button3.UseVisualStyleBackColor = True
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(125, 126)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(100, 20)
+        Me.TextBox1.TabIndex = 9
+        '
+        'TextBox2
+        '
+        Me.TextBox2.Location = New System.Drawing.Point(526, 126)
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.Size = New System.Drawing.Size(100, 20)
+        Me.TextBox2.TabIndex = 10
+        '
+        'TextBox3
+        '
+        Me.TextBox3.Location = New System.Drawing.Point(526, 157)
+        Me.TextBox3.Name = "TextBox3"
+        Me.TextBox3.Size = New System.Drawing.Size(100, 20)
+        Me.TextBox3.TabIndex = 11
+        '
+        'ComboBox1
+        '
+        Me.ComboBox1.FormattingEnabled = True
+        Me.ComboBox1.Items.AddRange(New Object() {"Tipo 1", "Tipo 2", "Tipo 3", "Tipo 4"})
+        Me.ComboBox1.Location = New System.Drawing.Point(125, 154)
+        Me.ComboBox1.Name = "ComboBox1"
+        Me.ComboBox1.Size = New System.Drawing.Size(121, 21)
+        Me.ComboBox1.TabIndex = 12
+        '
+        'MenuStrip1
+        '
+        Me.MenuStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.VaciarVectoresToolStripMenuItem, Me.VaciarDatagridToolStripMenuItem, Me.SalirToolStripMenuItem})
+        Me.MenuStrip1.Location = New System.Drawing.Point(0, 0)
+        Me.MenuStrip1.Name = "MenuStrip1"
+        Me.MenuStrip1.Size = New System.Drawing.Size(701, 24)
+        Me.MenuStrip1.TabIndex = 13
+        Me.MenuStrip1.Text = "MenuStrip1"
+        '
+        'VaciarVectoresToolStripMenuItem
+        '
+        Me.VaciarVectoresToolStripMenuItem.Name = "VaciarVectoresToolStripMenuItem"
+        Me.VaciarVectoresToolStripMenuItem.Size = New System.Drawing.Size(97, 20)
+        Me.VaciarVectoresToolStripMenuItem.Text = "Vaciar vectores"
+        '
+        'VaciarDatagridToolStripMenuItem
+        '
+        Me.VaciarDatagridToolStripMenuItem.Name = "VaciarDatagridToolStripMenuItem"
+        Me.VaciarDatagridToolStripMenuItem.Size = New System.Drawing.Size(98, 20)
+        Me.VaciarDatagridToolStripMenuItem.Text = "Vaciar Datagrid"
+        '
+        'SalirToolStripMenuItem
+        '
+        Me.SalirToolStripMenuItem.Name = "SalirToolStripMenuItem"
+        Me.SalirToolStripMenuItem.Size = New System.Drawing.Size(41, 20)
+        Me.SalirToolStripMenuItem.Text = "Salir"
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.BackColor = System.Drawing.SystemColors.ControlDarkDark
+        Me.Label5.Location = New System.Drawing.Point(21, 73)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(88, 13)
+        Me.Label5.TabIndex = 14
+        Me.Label5.Text = "BUSCAR PLACA"
+        '
+        'TextBox4
+        '
+        Me.TextBox4.Location = New System.Drawing.Point(125, 66)
+        Me.TextBox4.Name = "TextBox4"
+        Me.TextBox4.Size = New System.Drawing.Size(100, 20)
+        Me.TextBox4.TabIndex = 15
+        '
+        'Button4
+        '
+        Me.Button4.Location = New System.Drawing.Point(251, 65)
+        Me.Button4.Name = "Button4"
+        Me.Button4.Size = New System.Drawing.Size(98, 20)
+        Me.Button4.TabIndex = 16
+        Me.Button4.Text = "Buscar"
+        Me.Button4.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackgroundImage = CType(resources.GetObject("$this.BackgroundImage"), System.Drawing.Image)
+        Me.ClientSize = New System.Drawing.Size(701, 450)
+        Me.Controls.Add(Me.Button4)
+        Me.Controls.Add(Me.TextBox4)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.ComboBox1)
+        Me.Controls.Add(Me.TextBox3)
+        Me.Controls.Add(Me.TextBox2)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.Button3)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.DataGridView1)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.MenuStrip1)
+        Me.MainMenuStrip = Me.MenuStrip1
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.MenuStrip1.ResumeLayout(False)
+        Me.MenuStrip1.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents DataGridView1 As DataGridView
+    Friend WithEvents Button2 As Button
+    Friend WithEvents Button3 As Button
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents TextBox2 As TextBox
+    Friend WithEvents TextBox3 As TextBox
+    Friend WithEvents ComboBox1 As ComboBox
+    Friend WithEvents Column1 As DataGridViewTextBoxColumn
+    Friend WithEvents Column2 As DataGridViewTextBoxColumn
+    Friend WithEvents Column3 As DataGridViewTextBoxColumn
+    Friend WithEvents Column4 As DataGridViewTextBoxColumn
+    Friend WithEvents Column5 As DataGridViewTextBoxColumn
+    Friend WithEvents Column6 As DataGridViewTextBoxColumn
+    Friend WithEvents MenuStrip1 As MenuStrip
+    Friend WithEvents VaciarVectoresToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents VaciarDatagridToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents SalirToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents Label5 As Label
+    Friend WithEvents TextBox4 As TextBox
+    Friend WithEvents Button4 As Button
+End Class
---
+++ TAREAPARCIAL3/201800605_AUTOS/201800605_AUTOS/Form1.vb
@@ -0,0 +1,137 @@
+﻿Public Class Form1
+
+    Dim x As Integer = 0
+    Dim numero_placa(7) As String
+    Dim tipoAuto(7) As String
+    Dim cobroBase(7) As Double
+    Dim kilometraje_ini(7) As Double
+    Dim kilometraje_fin(7) As Double
+    Dim pagofinal(7) As Double
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+
+        If TextBox1.Text <> "" And TextBox2.Text <> "" And TextBox3.Text <> "" Then
+            Dim ini, fin, tot, pre, ex, pafin As Double
+            ini = CDbl(TextBox2.Text)
+            fin = CDbl(TextBox3.Text)
+            tot = fin - ini
+            ex = getExtra(tot)
+            pre = getPrecio()
+
+            pafin = pre + ex
+            If x < 7 Then
+                numero_placa(x) = TextBox1.Text
+                tipoAuto(x) = gettipo()
+                cobroBase(x) = pre
+                kilometraje_ini(x) = ini
+                kilometraje_fin(x) = fin
+                pagofinal(x) = pre + ex
+
+                DataGridView1.Rows.Add(TextBox1.Text, gettipo(), getPrecio(), ini, fin, pafin)
+                TextBox1.Clear()
+                TextBox2.Clear()
+                TextBox3.Clear()
+                x += 1
+            Else
+                MsgBox("el vector esta lleno y no se puede realizar la operacion")
+            End If
+        Else
+            MsgBox("no se pudo agregar")
+        End If
+
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs)
+
+    End Sub
+
+    Public Function gettipo() As String
+        MsgBox("Obtuve el tipoo")
+        If ComboBox1.SelectedIndex = 0 Then
+            Return "Tipo 1"
+        ElseIf ComboBox1.SelectedIndex = 1 Then
+            Return "Tipo 2"
+        ElseIf ComboBox1.SelectedIndex = 2 Then
+            Return "Tipo 3"
+        ElseIf ComboBox1.SelectedIndex = 3 Then
+            Return "Tipo 4"
+        Else
+            Return ""
+        End If
+    End Function
+
+    Public Function getPrecio() As Double
+
+        If ComboBox1.SelectedIndex = 0 Then
+            Return 500
+        ElseIf ComboBox1.SelectedIndex = 1 Then
+            Return 400
+        ElseIf ComboBox1.SelectedIndex = 2 Then
+            Return 300
+        ElseIf ComboBox1.SelectedIndex = 3 Then
+            Return 200
+        Else
+            Return 0
+        End If
+    End Function
+
+    Public Function getExtra(km As Double) As Double
+        MsgBox(km)
+        If km <= 50 Then
+            Return km * 3
+        ElseIf km >= 50 And km <= 70 Then
+            Return km * 4
+        ElseIf km > 70 Then
+            Return km * 5
+        Else
+            Return 0
+        End If
+    End Function
+
+    Private Sub VaciarDatagridToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles VaciarDatagridToolStripMenuItem.Click
+        Me.DataGridView1.Rows.Clear()
+    End Sub
+
+    Private Sub SalirToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles SalirToolStripMenuItem.Click
+        Me.Close()
+    End Sub
+
+    Private Sub VaciarVectoresToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles VaciarVectoresToolStripMenuItem.Click
+        For i = 0 To 6
+            numero_placa(i) = Nothing
+            tipoAuto(i) = Nothing
+            cobroBase(i) = 0
+            kilometraje_ini(i) = 0
+            kilometraje_fin(i) = 0
+            pagofinal(i) = 0
+        Next
+
+        x = 0
+    End Sub
+
+    Private Sub Button4_Click(sender As Object, e As EventArgs) Handles Button4.Click
+        Dim dato As Boolean = False
+
+        For i = 0 To x
+            If TextBox4.Text = numero_placa(i) Then
+                If dato = False Then
+                    Me.DataGridView1.Rows.Clear()
+                End If
+                dato = True
+                DataGridView1.Rows.Add(numero_placa(i), tipoAuto(i), cobroBase(i), kilometraje_ini(i), kilometraje_fin(i), pagofinal(i))
+
+            End If
+        Next
+        TextBox4.Clear()
+        If dato = False Then
+            MsgBox("no se encontro el dato")
+        End If
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        For i = 0 To x
+
+            DataGridView1.Rows.Add(numero_placa(i), tipoAuto(i), cobroBase(i), kilometraje_ini(i), kilometraje_fin(i), pagofinal(i))
+
+        Next
+    End Sub
+End Class
